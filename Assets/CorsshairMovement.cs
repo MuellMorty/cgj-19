1ff09b07f8435931eda002c778039cd7bdaac799
LordMaarten <maarten-schmidt@outlook.de>
2019-08-25 16:24:18 +0200

    refs #2: player mevement;

---
+++ Assets/CorsshairMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorsshairMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+
+        transform.position = new Vector3(mouse.x, mouse.y, 0.0f);
+    }
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    public float movementRunningSpeed = 10f;
 
     public Rigidbody2D rb;
 
@@ -16,9 +17,19 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
-        animator.SetFloat("horizontal", movement.x);
-        animator.SetFloat("vertical", movement.y);
-        animator.SetFloat("speed", movement.sqrMagnitude);
+        
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float speedMultiplier = isRunning ? 2 : 1;
+
+        float movementSpeed = Mathf.Clamp(movement.magnitude, 0.0f, 1.0f);
+        
+        animator.SetFloat("horizontal", Mathf.Clamp(mouse.x, -1, 1));
+        animator.SetFloat("vertical", Mathf.Clamp(mouse.y, -1, 1));
+        animator.SetFloat("speed", movementSpeed * speedMultiplier);
+        animator.SetBool("isCasting", Input.GetKey(KeyCode.T));
     }
 
     private void FixedUpdate()
