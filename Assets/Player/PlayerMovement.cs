95ad71d471cdeba6596770cf0eb2826b488e9964
LordMaarten <maarten-schmidt@outlook.de>
2019-08-31 13:50:31 +0200

    player movement, but not really

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,35 +7,70 @@ public class PlayerMovement : Destroyable
 {
     public float movementSpeed = 5f;
     public float movementRunningSpeed = 10f;
+    public float movementMalus = 0.3f;
+    
+    public bool isRunning = false;
+    public bool isCasting = false;
 
     public Rigidbody2D rb;
 
-    private Vector2 movement;
+    private Vector2 movementInput;
     public Animator animator;
+
+    public Vector2 diff;
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-
-        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        setState();
+            
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
         mouse = Camera.main.ScreenToWorldPoint(mouse);
+        diff = mouse - transform.position;
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float speedMultiplier = isRunning ? 2 : 1;
-
-        float movementSpeed = Mathf.Clamp(movement.magnitude, 0.0f, 1.0f);
-
-        animator.SetFloat("horizontal", Mathf.Clamp(mouse.x, -1, 1));
-        animator.SetFloat("vertical", Mathf.Clamp(mouse.y, -1, 1));
-        animator.SetFloat("speed", movementSpeed * speedMultiplier);
-        animator.SetBool("isCasting", Input.GetKey(KeyCode.T));
+        setAnimatorState();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        movement();
+    }
+
+    public void setAnimatorState()
+    {
+        animator.SetFloat("horizontal", Mathf.Clamp(diff.x, -1, 1));
+        animator.SetFloat("vertical", Mathf.Clamp(diff.y, -1, 1));
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isCasting", isCasting);
+    }
+
+    private void setState()
+    {
+        isCasting = Input.GetKey(KeyCode.T);
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+    }
+
+    private void movement()
+    {
+        rb.MovePosition(rb.position + movementInput * calcMovementSpeed() * Time.fixedDeltaTime);
+    }
+
+    private float calcMovementSpeed()
+    {
+        float currentMovementSpeed = movementSpeed;
+
+        if (isRunning)
+        {
+            currentMovementSpeed = movementRunningSpeed;
+        }
+
+        if (isCasting)
+        {
+            currentMovementSpeed *= movementMalus;
+        }
+
+        return currentMovementSpeed;
     }
 
     protected override void OnDeath()
